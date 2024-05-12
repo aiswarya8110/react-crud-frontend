a35be10df7d7efbded535f61bd6bac23130e0bf2
aiswarya8110 <saish8110@gmail.com>
2024-05-12 21:56:38 +0530

    reverted target

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://react-crud-backend-iyds.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: false
       }
     }
